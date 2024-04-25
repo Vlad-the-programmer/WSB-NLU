e482393b5d65936468885143d13e0024bb7d6c32
Vlad-the-programmer <vladyslavklymchuk1@gmail.com>
2024-04-25 14:37:12 +0200

    Committing new project

---
+++ TaskManager-master/src/MainApp.java
@@ -0,0 +1,10 @@
+
+public class MainApp{
+
+	public static void main(String[] args)
+	{
+		ToDoList ToDo1 = new ToDoList();
+		ToDo1.setVisible(true);					//Makes the frame visible to the user (default = false)	
+	}
+
+}
